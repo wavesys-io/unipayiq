6aad95774158640eed3777043723d8111247971f
Amar Alaa <x.net87@gmail.com>
2021-09-30 17:44:59 +0300

    publish to npm

--- example.js
+++ example.js
@@ -1,28 +1,19 @@
 const express = require('express');
-
-
-
-const app = express();
 const unipay = require("./unipayiq")
 
 require('dotenv').config();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
-
 // do your logic
 const myCallback = data => console.log(data)
 
-
 app.use("/unipay", unipay(myCallback))
-
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
 });
-
-
